15ae3d8d87dc87beae63f326eb786cce2c6bd919
PS-Ddevil <sinhapurushottam911@gmail.com>
2019-01-30 16:03:55 +0530

    #3

--- js/app.js
+++ js/app.js
@@ -2,10 +2,15 @@ const sportslist = document.querySelector('#sports');
 const pollist = document.querySelector('#politics');
 const litlist = document.querySelector('#lit');
 const techlist = document.querySelector('#tech');
+const form = document.querySelector('#form-input')
 
+//displaying the data
+
+//Sports List
 function rendersportslist(doc){
     let m_div = document.createElement('div');
     m_div.classList.add('row');
+    m_div.setAttribute('data-id',doc.id);
     let sec_div = document.createElement('div');
     sec_div.classList.add('col');
     sec_div.classList.add('m10');
@@ -30,6 +35,7 @@ function rendersportslist(doc){
     sportslist.appendChild(m_div);
 }
 
+//Literature List
 function renderlitlist(doc){
     let m_div = document.createElement('div');
     m_div.classList.add('row');
@@ -57,6 +63,7 @@ function renderlitlist(doc){
     litlist.appendChild(m_div);
 }
 
+//Politics List
 function renderpollist(doc){
     let m_div = document.createElement('div');
     m_div.classList.add('row');
@@ -84,6 +91,7 @@ function renderpollist(doc){
     pollist.appendChild(m_div);
 }
 
+//Tech List
 function rendertechlist(doc){
     let m_div = document.createElement('div');
     m_div.classList.add('row');
@@ -111,26 +119,91 @@ function rendertechlist(doc){
     techlist.appendChild(m_div);
 }
 
-db.collection('sports').get().then(snapshot => {
-    snapshot.docs.forEach(doc => {
-        rendersportslist(doc);
+//Get Data from Firestore
+// db.collection('sports').get().then(snapshot => {
+//     snapshot.docs.forEach(doc => {
+//         rendersportslist(doc);
+//     });
+// });
+
+// db.collection('lit').get().then(snapshot => {
+//     snapshot.docs.forEach(doc => {
+//         renderlitlist(doc);
+//     });
+// });
+
+// db.collection('politics').get().then(snapshot => {
+//     snapshot.docs.forEach(doc => {
+//         renderpollist(doc);
+//     });
+// });
+
+// db.collection('tech').get().then(snapshot => {
+//     snapshot.docs.forEach(doc => {
+//         rendertechlist(doc);
+//     });
+// });
+
+//Inserting Data into the firestore
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const genre = form.genre.value;
+    const password = form.pass.value;
+    if(password == "puru"){
+        db.collection(genre).add({
+            title: form.title.value,
+            content: form.content.value
+        });
+    }else{
+        alert("Wrong Password!");
+    }
+    $('#form-input').trigger("reset");
+});
+
+db.collection('sports').onSnapshot(snapshot => {
+    let changes = snapshot.docChanges();
+    changes.forEach(change => {
+        if(change.type == "added"){
+            rendersportslist(change.doc);
+        }else{
+            let todeldiv = sportslist.querySelector('[data-id='+change.doc.id+']');
+            sportslist.removeChild(todeldiv);
+        }
     });
 });
 
-db.collection('lit').get().then(snapshot => {
-    snapshot.docs.forEach(doc => {
-        renderlitlist(doc);
+db.collection('lit').onSnapshot(snapshot => {
+    let changes = snapshot.docChanges();
+    changes.forEach(change => {
+        if(change.type == "added"){
+            renderlitlist(change.doc);
+        }else{
+            let todeldiv = litlist.querySelector('[data-id='+change.doc.id+']');
+            litlist.removeChild(todeldiv);
+        }
     });
 });
 
-db.collection('politics').get().then(snapshot => {
-    snapshot.docs.forEach(doc => {
-        renderpollist(doc);
+db.collection('politics').onSnapshot(snapshot => {
+    let changes = snapshot.docChanges();
+    changes.forEach(change => {
+        if(change.type == "added"){
+            renderpollist(change.doc);
+        }else{
+            let todeldiv = pollist.querySelector('[data-id='+change.doc.id+']');
+            pollist.removeChild(todeldiv);
+        }
     });
 });
 
-db.collection('tech').get().then(snapshot => {
-    snapshot.docs.forEach(doc => {
-        rendertechlist(doc);
+db.collection('tech').onSnapshot(snapshot => {
+    let changes = snapshot.docChanges();
+    changes.forEach(change => {
+        if(change.type == "added"){
+            rendertechlist(change.doc);
+        }else{
+            let todeldiv = techlist.querySelector('[data-id='+change.doc.id+']');
+            techlist.removeChild(todeldiv);
+        }
     });
 });
